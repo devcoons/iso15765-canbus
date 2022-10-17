9e8d797b5744e66cdd1a7f211cf6a1446d3f8bc1
Io. D <41527442+devcoons@users.noreply.github.com>
2022-10-17 15:21:27 +0200

    Update lib_iqueue.c

--- src/lib_iqueue.c
+++ src/lib_iqueue.c
@@ -53,17 +53,16 @@
 
 i_status iqueue_init(iqueue_t* _queue, int _max_elements, size_t _element_size, void* _storage)
 {
-	if (_queue == NULL)
-		return I_ERROR;
-
-	memset(_storage, 0x00, _element_size * _max_elements);
-
-	_queue->element_size = _element_size;
-	_queue->max_elements = _max_elements;
-	_queue->first = (void*)0;
-	_queue->next = _queue->storage = _storage;
-
-	return I_OK;
+	if (_queue != NULL)
+	{	
+		memset(_storage, 0x00, _element_size * _max_elements);
+		_queue->element_size = _element_size;
+		_queue->max_elements = _max_elements;
+		_queue->first = (void*)0;
+		_queue->next = _queue->storage = _storage;
+		return I_OK;
+	}
+	return I_ERROR;
 }
 
 
@@ -74,56 +73,54 @@ volatile void* iqueue_get_next_enqueue(iqueue_t* _queue)
 
 i_status iqueue_advance_next(iqueue_t* _queue)
 {
-	if (_queue->first == _queue->next)
-		return I_FULL;
-
-	_queue->first = _queue->first == (void*)0 ? _queue->next : _queue->first;
-	_queue->next = (uint8_t*)_queue->next + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
-		? _queue->storage : (uint8_t*)_queue->next + _queue->element_size;
-
-	return I_OK;
+	if (_queue->first != _queue->next)
+	{	
+		_queue->first = _queue->first == (void*)0 ? _queue->next : _queue->first;
+		_queue->next = (uint8_t*)_queue->next + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
+			? _queue->storage : (uint8_t*)_queue->next + _queue->element_size;
+		return I_OK;
+	}
+	return I_FULL;
 }
 
 i_status iqueue_enqueue(iqueue_t* _queue, void* _element)
 {
-	if (_queue->first == _queue->next)
-		return I_FULL;
-
-	memmove((void*)_queue->next, (void*)_element, _queue->element_size);
-
-	_queue->first = _queue->first == (void*)0 ? _queue->next : _queue->first;
-	_queue->next = (uint8_t*)_queue->next + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
-		? _queue->storage : (uint8_t*)_queue->next + _queue->element_size;
-
-	return I_OK;
+	if (_queue->first != _queue->next)
+	{	
+		memmove((void*)_queue->next, (void*)_element, _queue->element_size);
+		_queue->first = _queue->first == (void*)0 ? _queue->next : _queue->first;
+		_queue->next = (uint8_t*)_queue->next + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
+			? _queue->storage : (uint8_t*)_queue->next + _queue->element_size;
+		return I_OK;
+	}
+	return I_FULL;
 }
 
 i_status iqueue_dequeue(iqueue_t* _queue, void* _element)
 {
-	if (_queue->first == (void*)0)
-		return I_EMPTY;
+	if (_queue->first != (void*)0)
+	{
+		memmove((void*)_element, (void*)_queue->first, _queue->element_size);
+		_queue->first = (uint8_t*)_queue->first + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
+			? _queue->storage : (uint8_t*)_queue->first + _queue->element_size;
+		_queue->first = _queue->first == _queue->next ? (void*)0 : _queue->first;
 
-	memmove((void*)_element, (void*)_queue->first, _queue->element_size);
-
-	_queue->first = (uint8_t*)_queue->first + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
-		? _queue->storage : (uint8_t*)_queue->first + _queue->element_size;
-	_queue->first = _queue->first == _queue->next ? (void*)0 : _queue->first;
-
-	return I_OK;
+		return I_OK;
+	}
+	return I_EMPTY;
 }
 
 void* iqueue_dequeue_fast(iqueue_t* _queue)
 {
-	if (_queue->first == (void*)0)
-		return NULL;
-
-	void* x = (void*)_queue->first;
-
-	_queue->first = (uint8_t*)_queue->first + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
-		? _queue->storage : (uint8_t*)_queue->first + _queue->element_size;
-	_queue->first = _queue->first == _queue->next ? (void*)0 : _queue->first;
-
-	return x;
+	if (_queue->first != (void*)0)
+	{	
+		void* x = (void*)_queue->first;
+		_queue->first = (uint8_t*)_queue->first + _queue->element_size == (uint8_t*)_queue->storage + (_queue->element_size * _queue->max_elements)
+			? _queue->storage : (uint8_t*)_queue->first + _queue->element_size;
+		_queue->first = _queue->first == _queue->next ? (void*)0 : _queue->first;
+		return x;
+	}
+	return NULL;
 }
 
 
